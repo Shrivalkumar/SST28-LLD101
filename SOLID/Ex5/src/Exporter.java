2ce69c1a0d81aa3226f4c40b09f00fd0e0cf1dd5
Shrivalkumar <shrival.24bcs10254@sst.scaler.com>
2026-02-23 02:05:35 +0530

    Standardize Exporter contract and fix LSP violations in PdfExporter

--- SOLID/Ex5/src/Exporter.java
+++ SOLID/Ex5/src/Exporter.java
@@ -1,4 +1,20 @@
 public abstract class Exporter {
-    // implied "contract" but not enforced (smell)
-    public abstract ExportResult export(ExportRequest req);
+        
+    public final ExportResult export(ExportRequest req){
+        if(req == null){
+            return new ExportResult(getContentType(), new byte[0]);
+        }
+        if (req.body != null && req.body.length() > maxLength()) {
+            throw new IllegalArgumentException(getFormatName() + " cannot handle content > " + maxLength() + " chars");
+        }
+        return generateExport(req);
+        
+    }
+    protected abstract ExportResult generateExport(ExportRequest req);
+
+    public int maxLength(){
+        return (int)1e9;
+    }
+    public abstract String getFormatName();
+    public abstract String getContentType();
 }
